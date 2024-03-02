0e40f74b9d32c97dbbbecd15c60dd58242baedf6
heibaiyuanfen <heibaiyuanfen@gmail.com>
2024-03-02 22:00:14 +0800

    GameManagerAPP

---
+++ GameManagerApp/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
